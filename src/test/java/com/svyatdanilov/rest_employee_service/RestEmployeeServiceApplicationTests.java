4a1d3da6c0a40b541836a4c3a3a7f84ccba1fe61
Svyat Danilov <danilov.svat@mail.ru>
2023-02-13 12:53:55 +0300

    Create master branch

---
+++ src/test/java/com/svyatdanilov/rest_employee_service/RestEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svyatdanilov.rest_employee_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
