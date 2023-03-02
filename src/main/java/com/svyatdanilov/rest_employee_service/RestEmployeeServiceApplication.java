70c40f94f74f40ce27a66a923e39561e35ca4f3f
Svyat Danilov <danilov.svat@mail.ru>
2023-03-02 15:44:36 +0300

    off security, add exceptionhandling

--- src/main/java/com/svyatdanilov/rest_employee_service/RestEmployeeServiceApplication.java
+++ src/main/java/com/svyatdanilov/rest_employee_service/RestEmployeeServiceApplication.java
@@ -2,8 +2,10 @@ package com.svyatdanilov.rest_employee_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class RestEmployeeServiceApplication {
 
 	public static void main(String[] args) {
