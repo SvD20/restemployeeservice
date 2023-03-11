9b9ca1b4c344404842ace9cc7765620b15fe78e8
Svyat Danilov <danilov.svat@mail.ru>
2023-03-11 22:51:18 +0300

    add basic authentication for 2 users

---
+++ src/main/java/com/svyatdanilov/rest_employee_service/securityconfig/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.svyatdanilov.rest_employee_service.securityconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Класс-шифровальщик пароля
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();//никак не шифрует
+    }
+
+
+    //Настройка аутиэнфикейшенманагера через настройку его билдера
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("u1")
+                .password("p1")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("u2")
+                .password("p2")
+                .authorities("ROLE_USER");
+    }
+
+
+
+}
